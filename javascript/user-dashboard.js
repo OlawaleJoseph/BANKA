5caca03dbc42bb43786df4328398e93744b843b7
Olawale <adedekojoseph@gmail.com>
2019-03-29 20:18:56 +0100

    created user dashboard

---
+++ javascript/user-dashboard.js
@@ -0,0 +1,43 @@
+const closeBtn = document.querySelector("#close-btn");
+const sideNav = document.querySelector("#side-menu");
+const openSideNav = document.querySelector("#bars");
+const links = document.querySelectorAll('.nav-links');
+const divs = document.querySelectorAll('.display-div');
+
+const openNcloseSideNav = () => {
+    closeBtn.addEventListener("click", () => {
+        sideNav.style.display = "none";
+    });
+    
+    openSideNav.addEventListener("click", () => {
+        sideNav.style.display = "block";
+    })
+}
+
+const sideMenu = function(){
+    links.forEach(function(link){
+        link.addEventListener("click", function(){
+            divs.forEach(function(div){
+                div.style.display = "none";
+            });
+            const id = this.getAttribute("data-id");
+            if(id == '#transaction'){
+                document.querySelector(id).style.display = "grid";
+                if(window.innerWidth < 768){
+                    sideNav.style.display = "none";
+                }
+            }else{
+                document.querySelector(id).style.display = "block";
+                if(window.innerWidth < 768){
+                    sideNav.style.display = "none";
+                }
+            }
+            
+            
+    
+        });
+    })
+}
+
+    openNcloseSideNav();
+    sideMenu();
