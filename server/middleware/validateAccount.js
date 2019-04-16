ed193433388c44438e8aadc46a81e1135abdd339
Olawale <adedekojoseph@gmail.com>
2019-04-16 11:07:55 +0100

    bug(validation-response): update response status of noMultipleAccount middleware [Delivers #165378306]

--- server/middleware/validateAccount.js
+++ server/middleware/validateAccount.js
@@ -3,8 +3,8 @@ import accountModel from '../models/account';
 export const noMultipleAccounts = (req, res, next) => {
     const ownsAccount = accountModel.accountsDb.find(acc => acc.owner == req.user.id)
     if(ownsAccount){
-        return res.status(400).json({
-            "status": 400,
+        return res.status(409).json({
+            "status": 409,
             "error": "User already has an account"
         });
     }else{
